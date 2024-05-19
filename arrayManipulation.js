5566350fc55afe115906a300d91e7b18a65d3066
Andrew Tano <131122540+prolificdee99@users.noreply.github.com>
2024-05-19 12:50:39 +0000

    Update arrayManipulation.js
    Added formatArrayStrings function to modify strings based on corresponding numbers

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,14 @@
 function processArray(arr) {
-    return arr.map(num => num % 2 === 0 ? num * num : num * 3);
+    return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
   }
+
+  function formatArrayStrings(stringsArray, numbersArray) {
+    return stringsArray.map((str, index) => {
+        const num = numbersArray[index];
+        if (num % 2 === 0) {
+            return str.toUpperCase(); // Capitalize if number is even
+        } else {
+            return str.toLowerCase(); // Convert to lowercase if number is odd
+        }
+    });
+}
